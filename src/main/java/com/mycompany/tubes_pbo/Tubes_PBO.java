3d71953a3ed93a242056e0d7ab0d5be506a021b0
NovelShiffa22 <shffaocta@gmail.com>
2025-04-19 15:10:11 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/tubes_pbo/Tubes_PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tubes_pbo;
+
+/**
+ *
+ * @author novel
+ */
+public class Tubes_PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
