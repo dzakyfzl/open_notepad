90ad66aa6cdaa1d9ab2c793e55a733134743ba40
dzakyfzl <160983103+dzakyfzl@users.noreply.github.com>
2025-05-08 13:30:45 +0700

    add validation

---
+++ src/main/java/com/kelompok5/open_notepad/AccountController.java
@@ -0,0 +1,90 @@
+package com.kelompok5.open_notepad;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+
+
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AccountController extends Validation {
+
+    //Authentication API
+    @PostMapping("/signin")//login
+    public ResponseEntity<Map<String,String>> signIn(@RequestBody Map<String,String> requestData,HttpServletRequest request, HttpSession session) {
+        String username = requestData.get("username");
+        String password = requestData.get("password");
+        String IPString = request.getRemoteAddr();
+        String sessionID = session.getId();
+        session.setMaxInactiveInterval(60*60*24); // 1 day
+        if(username == null || password == null) {
+            return ResponseEntity.badRequest().body(Map.of("message", "Username and password are required"));
+        }
+        session.setAttribute("username", username);
+
+        return ResponseEntity.ok().body(Map.of("message", "User logged in successfully"));
+    }
+
+    @PostMapping("/signup")//register
+    public ResponseEntity<Map<String,String>> signUp(@RequestBody Map<String,String> requestData,HttpServletRequest request, HttpSession session) {
+        String username = requestData.get("username");
+        String password = requestData.get("password");
+        String email = requestData.get("email");
+        String firstName = requestData.get("firstName");
+        String lastName = requestData.get("lastName");
+        if(username == null || password == null || email == null || firstName == null || lastName == null) {
+            return ResponseEntity.badRequest().body(Map.of("message", "All fields are required"));
+        }
+        session.setAttribute("username", username);
+        return ResponseEntity.ok().body(Map.of("message", "User registered successfully"));
+    }
+
+    @PostMapping("/logout")//logout
+    public ResponseEntity<Map<String,String>> checkSession(HttpServletRequest request, HttpSession session) {
+        if(isSessionValid(session, request)) {
+            return ResponseEntity.badRequest().body(Map.of("message", "User not logged in"));
+        }
+        return ResponseEntity.ok().body(Map.of("message", "User logged in successfully"));
+    }
+
+    @PostMapping("/edit")//edit profile
+    public ResponseEntity<Map<String,String>> editProfile(@RequestBody Map<String,String> requestData,HttpServletRequest request, HttpSession session) {
+        if(isSessionValid(session, request)) {
+            return ResponseEntity.badRequest().body(Map.of("message", "User not logged in"));
+        }
+        return ResponseEntity.ok().body(Map.of("message", "User profile updated successfully"));
+    }
+
+    @PostMapping("/delete")//delete account
+    public ResponseEntity<Map<String,String>> deleteAccount(HttpServletRequest request, HttpSession session) {
+        if(isSessionValid(session, request)) {
+            return ResponseEntity.badRequest().body(Map.of("message", "User not logged in"));
+        }
+        return ResponseEntity.ok().body(Map.of("message", "User account deleted successfully"));
+    }
+
+    @GetMapping("/info")//get user info
+    public ResponseEntity<Map<String,String>> getUserInfo(HttpServletRequest request, HttpSession session) {
+        if(isSessionValid(session, request)) {
+            return ResponseEntity.badRequest().body(Map.of("message", "User not logged in"));
+        }
+        String username = (String) session.getAttribute("username");
+        //Database querry to get user info
+        String email = "";
+        String firstName = "";
+        String lastName = "";
+        return ResponseEntity.ok().body(Map.of( "username", username,"email",email,"firstName",firstName,"lastName",lastName));
+    }
+
+}
--- src/main/java/com/kelompok5/open_notepad/PageController.java
+++ src/main/java/com/kelompok5/open_notepad/PageController.java
@@ -2,23 +2,24 @@ package com.kelompok5.open_notepad;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class PageController{
+public class PageController extends Validation {
     @GetMapping("/")
-    public String home(HttpSession session) {
+    public String home(HttpSession session, HttpServletRequest request) {
         // Check if the user is logged in
-        if (session.getAttribute("username") == null) {
+        if (isSessionValid(session, request)) {
             // If not logged in, redirect to the main page
             return "redirect:/login";
         }
         return "index";
     }
     @GetMapping("/login")
-    public String login(HttpSession session) {
+    public String login(HttpSession session, HttpServletRequest request) {
         // Check if the user is logged in
-        if (session.getAttribute("username") != null) {
+        if (isSessionValid(session, request)) {
             // If logged in, redirect to the main page
             return "redirect:/";
         }
